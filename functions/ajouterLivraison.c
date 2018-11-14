caa37005c1af054ae0b8cf7b23e882078e4302ee
Le Roy Tanguy <e1702832@ens-ssilo-0198.univ-ubs.fr>
2018-11-14 14:50:13 +0100

    création d'une fonction pour ajouter des livraisons

---
+++ functions/ajouterLivraison.c
@@ -0,0 +1,18 @@
+#include "../structures.c"
+
+
+void ajouterLivraison(ptlivraison pdebut,ptville,int chauffeur,int capacite,int disponible){
+ ptlivraison py=(ptlivraison)malloc(sizeof(tlivraison);
+ ptlivraison px=pdebut;
+
+
+while(px->livraisonSuivante !=NULL){
+
+    px=px->livraisonSuivante;
+}
+
+
+py->livraisonSuivante=px->livraisonSuivante;
+px->livraisonSuivante;
+py->chauffeur=chauffeur;
+
