07a3e66affb2b72d82e7000a6936ae0175959e28
Le Roy Tanguy <e1702832@ens-ssilo-0198.univ-ubs.fr>
2018-11-14 15:30:29 +0100

    ajout livraison dans une ville

--- functions/ajouterLivraison.c
+++ functions/ajouterLivraison.c
@@ -1,18 +1,46 @@
 #include "../structures.c"
+#include "rechercheVille.c"
 
+void ajouterLivraison(ptlivraison plivraisonDebut, int numVille, int chauffeur, int capacite, int disponible){
 
-void ajouterLivraison(ptlivraison pdebut,ptville,int chauffeur,int capacite,int disponible){
- ptlivraison py=(ptlivraison)malloc(sizeof(tlivraison);
- ptlivraison px=pdebut;
+        ptlivraison py=(ptlivraison)malloc(sizeof(tlivraison);
+        ptlivraison px=plivraisonDebut;
+        ptville ptest=(ptville)malloc(sizeof(tville);
+        int postal;
 
+        ptville pz = rechercheVille(numVille);
+        ptest = rechercheVille(postal);
+        if(ptest == NULL)
+        {
+            printf("Cette ville n'existe pas");
+        }
+        else
+        {
+            pz = ptest;
+        }
 
-while(px->livraisonSuivante !=NULL){
+        while(px->livraisonSuivante->livraisonSuivante != NULL)
+        {
+            px = px->livraisonSuivante;
+        }
 
-    px=px->livraisonSuivante;
-}
+        py->livraisonSuivante = px->livraisonSuivante;
+        px->livraisonSuivante = py;
+        py->chauffeur = chauffeur;
+        py->capacite = capacite;
+        py->disponible = disponible;
 
-
-py->livraisonSuivante=px->livraisonSuivante;
-px->livraisonSuivante;
-py->chauffeur=chauffeur;
-
+        if(py->disponible == 0)
+        {
+            printf("Où va la livraison : ");
+            scanf("%d", &postal);
+            ptest = rechercheVille(postal);
+            if(ptest == NULL)
+            {
+                printf("Cette ville n'existe pas");
+            }
+            else
+            {
+                py->enLivraison = ptest;
+            }
+        }
