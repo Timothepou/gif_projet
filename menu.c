4e6e5a17381e343332cb1c6615baf8d02cf961d1
Timothepou <44804419+Timothepou@users.noreply.github.com>
2018-12-03 12:16:01 +0100

    Add files via upload

--- menu.c
+++ menu.c
@@ -18,27 +18,19 @@ void afficherVille(ptville pdebutVille){
     while(px->villeSuivante->villeSuivante != NULL)
     {
         px = px->villeSuivante;
-        printf("[OK] Ville n¬∞%d\n", px->numVille);
+        printf("[OK] Ville n∞%d\n", px->numVille);
     }
 }
 
 int main(){
+	
+  system("color 2A");
 
   char T[1];
   int a = 0;
-  printf("[1]- Ajouter une ville. \n"
-         "[2]- Modifier une ville. \n"
-         "[3]- Supprimer une ville. \n"
-         "[4]- Ajouter une livraison dans une ville. \n"
-         "[5]- Supprimer une livrasion dans une ville. \n"
-         "[6]- Modifier une livraison dans une ville. \n"
-         "[7]- Rechercher un chauffeur disponible ayant une capacit√© N dans une ville V.\n"
-         "[8]- Rechercher un chauffeur disponible ayant une capacit√© N dans toutes les villes. \n"
-         "[9]- Afficher les villes. \n"
-         "[0]- Quitter. \n\n"
-         "Choissisez l'op√©ration √† effectuer: ");
-
+  printf("\n[1]- Ajouter une ville. \n[2]- Modifier une ville. \n[3]- Supprimer une ville. \n[4]- Ajouter une livraison dans une ville. \n[5]- Supprimer une livrasion dans une ville. \n[6]- Modifier une livraison dans une ville. \n[7]- Rechercher un chauffeur disponible ayant une capacite N dans une ville V. \n[8]- Rechercher un chauffeur disponible ayant une capacite N dans toutes les villes. \n[9]- TransfÈrer un chauffeur X dune ville A a† une ville B. \n[0]- Quitter. \n\nChoissisez l'operation a effectuer: ");
   scanf("%c", &T[0]);
+  system("cls");
   if( ((int)T[0] >= 48) && ((int)T[0] <= 57) )
     printf("");
   else
@@ -47,7 +39,7 @@ int main(){
   a = (int)T[0] - 48;
 
   if(a <= 0 || a > 9){
-        printf("[***] Fermeture du programme."); // TODO: G√©rer les erreurs, ex: scanf un caract√®re
+        printf("[***] Fermeture du programme."); // TODO: GÈrer les erreurs, ex: scanf un caractËre
         return 0;
   }
 
@@ -59,6 +51,19 @@ int main(){
   pdebutVille->villePrecedente = NULL;
   pfinVille->villePrecedente = pdebutVille;
   pfinVille->villeSuivante = NULL;
+  
+  ptville pdebutVillePoubelle = (ptville)malloc(sizeof(tville));
+  ptville pfinVillePoubelle= (ptville)malloc(sizeof(tville));
+  pdebutVillePoubelle->villeSuivante = pfinVillePoubelle;
+  pdebutVillePoubelle->villePrecedente = NULL;
+  pfinVillePoubelle->villePrecedente = pdebutVillePoubelle;
+  pfinVillePoubelle->villeSuivante = NULL;
+  
+  ptlivraison pdebutLivraisonPoubelle = (ptlivraison)malloc(sizeof(tlivraison));
+  ptlivraison pfinLivraisonPoubelle = (ptlivraison)malloc(sizeof(tlivraison));
+  pdebutLivraisonPoubelle->livraisonSuivante = pfinLivraisonPoubelle;
+  pfinLivraisonPoubelle->livraisonSuivante = NULL;
+  
 
   int numVille, newnumVille, nbChauffeur, postal, capacite;
 
@@ -67,13 +72,13 @@ int main(){
     switch(a)
     {
       case 1:
-        printf("Quel est le code postal de la ville : ");
+        printf("[?] Quel est le code postal de la ville : ");
         scanf("%d", &postal);
-        ajouterVille(pdebutVille, postal);
+        ajouterVille(pdebutVille, pdebutVillePoubelle, postal);
         break;
 
       case 2:
-        printf("Quelle ville voulez-vous modifier : ");
+        printf("[?] Quelle ville voulez-vous modifier : ");
         scanf("%d", &numVille);
         modifierVille(pdebutVille, numVille);
         break;
@@ -81,56 +86,47 @@ int main(){
       case 3:
         printf("[?] Quelle ville voulez-vous supprimez : ");
         scanf("%d", &numVille);
-        supprimerVille(pdebutVille, numVille);
+        supprimerVille(pdebutVille, pdebutVillePoubelle, numVille);
         break;
 
       case 4:
-        printf("Dans quelle ville ajouter la livraison : ");
+        printf("[?] Dans quelle ville ajouter la livraison : ");
         scanf("%d", &numVille);
         ajouterLivraison(pdebutVille, numVille);
         break;
 
       case 5:
-      	printf("Dans quelle ville se trouve la livraison ‡ supprimer : ");
+      	printf("[?] Dans quelle ville se trouve la livraison ‡ supprimer : ");
       	scanf("%d", &numVille);
       	supprimerLivraison(pdebutVille, numVille);
         break;
 
       case 6:
-        printf("Dans quelle ville se trouve la livraison : ");
+        printf("[?] Dans quelle ville se trouve la livraison : ");
         scanf("%d", &numVille);
         modifierLivraison(pdebutVille, numVille);
         break;
 
       case 7:
-      	printf("Dans quelle ville rechercher une livraison: ");
+      	printf("[?] Dans quelle ville rechercher une livraison: ");
       	scanf("%d", &numVille);
       	rechercheChauffeurCapacite(pdebutVille, numVille);
         break;
 
       case 8:
-        printf("Capacite de la livraison : ");
+        printf("[?] Capacite de la livraison : ");
       	scanf("%d", &capacite);
       	rechercheChauffeurCapaciteAll(pdebutVille, capacite);
         break;
 
       case 9:
-        afficherVille(pdebutVille);
+        afficherVille(pdebutVillePoubelle);
         break;
      }
 
-     printf("\n[1]- Ajouter une ville. \n"
-            "[2]- Modifier une ville. \n"
-            "[3]- Supprimer une ville. \n"
-            "[4]- Ajouter une livraison dans une ville. \n"
-            "[5]- Supprimer une livrasion dans une ville. \n"
-            "[6]- Modifier une livraison dans une ville. \n"
-            "[7]- Rechercher un chauffeur disponible ayant une capacit√© N dans une ville V. \n"
-            "[8]- Rechercher un chauffeur disponible ayant une capacit√© N dans toutes les villes. \n"
-            "[9]- Transf√©rer un chauffeur X dune ville A √† une ville B. \n"
-            "[0]- Quitter. \n\n"
-            "Choissisez l'op√©ration √† effectuer: ");
+     printf("\n[1]- Ajouter une ville. \n[2]- Modifier une ville. \n[3]- Supprimer une ville. \n[4]- Ajouter une livraison dans une ville. \n[5]- Supprimer une livrasion dans une ville. \n[6]- Modifier une livraison dans une ville. \n[7]- Rechercher un chauffeur disponible ayant une capacite N dans une ville V. \n[8]- Rechercher un chauffeur disponible ayant une capacite N dans toutes les villes. \n[9]- TransfÈrer un chauffeur X dune ville A √† une ville B. \n[0]- Quitter. \n\nChoissisez l'operation a effectuer: ");
      scanf("%d", &a);
+     system("cls");
      if(a <= 0 || a > 9){
         printf("[***] Fermeture du programme.");
         return 0;
--- structures.c
+++ structures.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <dos.h>
 
 typedef struct livraison tlivraison;
 typedef struct ville tville;
